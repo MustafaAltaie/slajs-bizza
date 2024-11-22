78c5e0cba8108a504a375259190c7c08e6ee9575
mustafa altaie <mustafaphoto111@gmail.com>
2024-11-22 13:54:27 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <MenuView />
+  </main>
+</template>
+
+<script setup>
+import MenuView from './views/MenuView.vue';
+</script>
+
+<style>
+
+</style>
---
+++ src/composables/useMenu.js
@@ -0,0 +1,34 @@
+import { ref } from 'vue';
+import axios from 'axios';
+
+export function useMenu() {
+  const menu = ref([]);
+  const loading = ref(false);
+  const error = ref(null);
+
+  const fetchMenu = async () => {
+    loading.value = true;
+    error.value = null;
+    try {
+      const response = await axios.get('https://6ldruff9ul.execute-api.eu-north-1.amazonaws.com/menu', {
+        headers: { 'x-zocom': 'sp-7BTxHCyHhzIME5TI' },
+      });
+
+      console.log('API Response:', response.data);
+
+      if (Array.isArray(response.data.items)) {
+        menu.value = response.data.items;
+      } else {
+        menu.value = [];
+        console.error('Unexpected response format');
+      }
+    } catch (err) {
+      error.value = 'Failed to load menu. Please try again.';
+      console.error('API error:', err);
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return { menu, loading, error, fetchMenu };
+}
