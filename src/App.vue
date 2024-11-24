a14b4b6fd62e31abbcfd76bcca5a56b725da6ac5
mustafa altaie <mustafaphoto111@gmail.com>
2024-11-24 20:31:20 +0100

    Second commit

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-import MenuView from './views/MenuView.vue';
+import MenuView from './views/MenuView.vue'
 </script>
 
 <style>
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,94 +1,141 @@
-<template>
-    <div>
-      <p v-if="loading">Loading menu...</p>
-    
-      <p v-if="error">{{ error }}</p>
-    
-      <div v-else>
-        <div v-for="(items, type) in groupedMenu" :key="type" class="menu-section">
-            <h1>Slajs {{ type }} Menu</h1>
-          <ul>
-            <li v-for="item in items" :key="item.id">
-              <h3>{{ item.name }} - {{ item.price }} SEK</h3>
-              <p>{{ item.description }}</p>
-              <small>Ingredients: {{ item.toppings && Array.isArray(item.toppings) ? item.toppings.join(', ') : 'No toppings' }}</small>
-              <img :src="item.imgUrl" alt="Image of {{ item.name }}" width="150">
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </template>
-
 <script setup>
-import { ref, computed, onMounted } from 'vue';
-import { useMenu } from '../composables/useMenu';
+import { computed, onMounted, ref } from 'vue'
+import { useMenu } from '../composables/useMenu'
 
-const { menu, loading, error, fetchMenu } = useMenu();
+const { menu, loading, error, fetchMenu } = useMenu()
+const typeList = ref([])
+
+onMounted(() => {
+  fetchMenu()
+})
 
 const groupedMenu = computed(() => {
   if (menu.value && Array.isArray(menu.value)) {
     const grouped = menu.value.reduce((acc, item) => {
-      acc[item.type] = acc[item.type] || [];
-      acc[item.type].push(item);
-      return acc;
-    }, {});
-    console.log('Grouped Menu:', grouped);
-    return grouped;
+      acc[item.type] = acc[item.type] || []
+      acc[item.type].push(item)
+      typeList.value = Object.keys(acc)
+      return acc
+    }, {})
+    return grouped
   }
-  return {};
-});
+  return {}
+})
 
-onMounted(() => {
-  fetchMenu();
-});
+const scrollTo = (id) => {
+  const targetElement = document.getElementById(id)
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: 'smooth' })
+  }
+}
 </script>
+
+<template>
+  <div>
+    <h3 v-if="loading">Loading menu...</h3>
+  
+    <p v-if="error">{{ error }}</p>
+  
+    <div v-else>
+      <ol>
+        <li v-for="type in typeList" :key="type" @click="scrollTo(`${type}Menu`)">{{ type.toUpperCase() }}</li>
+      </ol>
+      <div v-for="(items, type) in groupedMenu" :key="type" class="menu-section">
+        <h1>{{ type.toUpperCase() }} MENU</h1>
+        <ul :id="`${type}Menu`">
+          <li v-for="item in items" :key="item.id">
+            <h3>{{ item.name }} - <span>{{ item.price }} SEK</span></h3>
+            <p>{{ item.description }}</p>
+            <small><span>Ingredients:</span> {{ item.toppings && Array.isArray(item.toppings) ? item.toppings.join(', ') : 'No toppings' }}</small>
+            <img :src="item.imgUrl" alt="Image of {{ item.name }}" width="150">
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+
 
 <style>
 *{
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
 }
 body{
-    background-image: url('https://cdn.pixabay.com/photo/2014/09/17/20/26/restaurant-449952_1280.jpg');
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-attachment: fixed;
+  background-image: url('https://cdn.pixabay.com/photo/2014/09/17/20/26/restaurant-449952_1280.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-attachment: fixed;
 }
 h1{
-    background: #ffffffaa;
-    padding: 30px;
-    text-align: center;
+  background: #ffffffaa;
+  padding: 30px;
+  text-align: center;
 }
 ul{
-    list-style: none;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    gap: 50px;
-    background: #ffffffaa;
-    width: 100%;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 50px;
+  background: #ffffffaa;
+  width: 100%;
+  padding-bottom: 50px;
 }
-li{
-    width: 300px;
-    background: #eeeeeec9;
-    box-shadow: 0 0 10px #00000033;
-    padding: 20px;
-    border-radius: 20px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    gap: 20px;
-    cursor: pointer;
-    transition: 0.3s;
+ul > li{
+  width: 300px;
+  background: #eeeeeec9;
+  box-shadow: 0 0 10px #00000011;
+  padding: 20px;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  transition: 0.3s;
 }
-li:hover{
-    transform: translateY(-10px);
+ul > li:hover{
+  background: #eeeeee;
+  box-shadow: 0 0 10px #00000044;
+}
+p{
+  color: #555;
 }
 h3{
-    color: blue;
+  color: #07a;
+}
+h3 > span{
+  color: #057;
+}
+small{
+  color: #777;
+}
+small > span{
+  color: #111;
+  font-weight: bold;
+}
+ol{
+  position: fixed;
+  background: #fff;
+  list-style: none;
+  display: flex;
+  border-radius: 10px;
+  overflow: hidden;
+  transform: translate(10px, 10px);
+  border: solid 1px #aaa;
+}
+ol > li{
+  padding: 15px;
+  background: #00000014;
+  border-right: solid 1px #aaa;
+  cursor: pointer;
+  user-select: none;
+  margin-right: -1px;
 }
 </style>
